75c04d96662d1ee507bfe6a1ec22254c33ab9279
Your Nameronald <you@example.comronald260986@gmail.com>
2022-10-22 17:12:10 -0500

    commit inicial

---
+++ index.js
@@ -0,0 +1,13 @@
+const fn = () => {
+    const miString = "soy un string!"
+    let b = miString + "muy feliz"
+    console.log(b)
+}
+
+fn()
+
+const clickeado = () => {
+    let a = "chanchito feliz"
+
+    console.log(a)
+}
